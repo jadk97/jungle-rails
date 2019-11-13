f80d83c7d50026caafa5a1fc5acea11f023f10a5
jad <jad_dis@hotmail.com>
2019-11-13 19:28:11 +0000

    populated the admin dashboard page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.order(id: :desc).all
+    puts @products.count
+    puts @products.distinct.count('category_id')
   end
 end
